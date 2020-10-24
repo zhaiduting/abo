047c6627e50e30aa4456b498490aa4ee5decc79e
zhaiduting <zhaiduting@163.com>
2020-10-24 18:09:16 +0800

    master must be a function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function ext(master, closet) {
     function f() {
-        if (typeof master === 'function')
-            return master.call(this, ...arguments);
+        return master.call(this, ...arguments);
     }
 
     f.closet = closet instanceof Array ? [...closet] : [];
