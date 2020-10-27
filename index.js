1f4e5a5389d4a4b7d8cb0177e950ecf282b6375d
zhaiduting <zhaiduting@163.com>
2020-10-27 12:36:28 +0800

    (chief)

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 function ext(master) {
     function f() {
         if (typeof master === 'function')
-            return master.call(f, ...arguments);
+            return master.call(f, ...arguments);    //Error: call(this, ...arguments)
     }
 
     f.closet = [];
     f.wear = function () {
-        f.closet.push(...arguments);
+        this.closet.push(...arguments);
     };
 
     f.assign = function () {
-        Object.assign(f, ...arguments);
+        Object.assign(this, ...arguments);
     };
 
-    f.grow = function () {
-        let g = ext(master);
-        g.wear(f.closet);
-        g.assign(f);
+    f.grow = function (chief) {
+        let g = ext(chief || master);
+        g.assign(this);
+        g.closet = [];
+        g.wear(...this.closet);
         return g;
     };
 
