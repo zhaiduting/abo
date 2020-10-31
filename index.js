4020621b500c5b1ed49f908ca069179894096c5f
zhaiduting <zhaiduting@163.com>
2020-10-31 12:40:08 +0800

    g.use(f)

--- index.js
+++ index.js
@@ -9,30 +9,58 @@ function abo(mind) {
         Array.prototype.push.apply(this.closet, arguments);
     };
 
-    f.assign = function () {
-        var i, arg;
-        var n = arguments.length;
-        for (i = 0; i < n; i++) {
-            arg = arguments[i];
-            for (var key in arg) {
-                if (key === 'closet') {
-                    Array.prototype.push.apply(this.closet, arg.closet);
-                } else if (Object.hasOwnProperty.call(arg, key)) {
-                    this[key] = arg[key];
-                }
-            }
-        }
-    };
-
     f.grow = function (idea) {
         var g = abo(idea || mind);
-        g.assign(this);
+        g.use(this);
         return g;
+    };
+
+    f.use = function (funcArrObj, willExpandExisted) {
+        var replace = !willExpandExisted;
+        var type = funcArrObj instanceof Array ? 'array' : typeof funcArrObj;
+        if (type === 'array') {
+            if (replace)
+                this.closet = [];
+            Array.prototype.push.apply(this.closet, funcArrObj);
+        } else if (type === 'object' || type === 'function') {
+            for (var key in funcArrObj) {
+                if (!Object.hasOwnProperty.call(funcArrObj, key))
+                    continue;
+                assign(this, key, funcArrObj[key], replace);
+            }
+        }
     };
 
     f.abo = abo;
 
     return f;
+}
+
+function assign(target, key, right, replace) {
+    var typeOfLeft = target[key] instanceof Array ? 'array' : typeof target[key];
+    var typeOfRight = right instanceof Array ? 'array' : typeof right;
+    switch (typeOfRight) {
+        case 'array':
+            if (replace || typeOfLeft !== 'array')
+                target[key] = [];
+            Array.prototype.push.call(target[key], right);
+            break;
+        case 'object':
+            if (replace || typeOfLeft !== 'object' || typeOfLeft !== 'array')
+                target[key] = {};
+            set(target[key], right);
+            break;
+        default:
+            target[key] = right;
+    }
+}
+
+function set(left, right) {
+    for (var k in right) {
+        if (!Object.hasOwnProperty.call(right, k))
+            continue;
+        left[k] = right[k];
+    }
 }
 
 export default abo;
